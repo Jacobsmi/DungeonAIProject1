616fd360271c2ca54e550d76424b38216e15ce8b
Michael Jacobs <jacobsmi@merrimack.edu>
2019-02-18 14:19:08 -0500

    Tree Working

--- src/csc3335_project1/AIProgram.java
+++ src/csc3335_project1/AIProgram.java
@@ -3,7 +3,9 @@ package csc3335_project1;
 import csc3335.dungeon_crawl.Dungeon;
 import csc3335.dungeon_crawl.Room;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class AIProgram {
@@ -17,13 +19,20 @@ public class AIProgram {
         Node head = new Node(r, d.containsSword(r), d.containsExit(r));
         tree = new Tree(head);
         getRoomNeighbors(head);
-
+    }
+    public void searchTree(){
+        Node swordRoom = findSwordRoom(tree.head);
+        List<Node> path = new ArrayList<>();
+        List<Node> finalPath;
+        finalPath = searchForSword(tree.head, swordRoom, path);
+        for(Node n: finalPath){
+            System.out.println(n.room.ROOM_ID);
+        }
     }
     public void getRoomNeighbors(Node n){
         for(Room t : n.room.getNeighbors()){
             Node node = new Node(t, d.containsSword(t), d.containsExit(t));
             if(tree.isNotNode(node)){
-                //System.out.println(n.room.ROOM_ID+" Neighbors "+ t.ROOM_ID);
                 n.children.add(node);
                 tree.addNode(node);
             }
@@ -38,20 +47,27 @@ public class AIProgram {
                 getRoomNeighbors(node);
             }
         }
-        //n.printChildren();
     }
-    //IterativeDeepeningDepthFirstSearch
-    public void searchForSword(){
-        Node goal = null;
-        Set<Node> current = new HashSet<>();
-        current.add(tree.head);
-        boolean atGoal = false;
+    public Node findSwordRoom(Node head){
         for(Node n : tree.allNodes){
             if(n.hasSword == true){
-                goal = n;
+                return n;
             }
         }
-        while(!atGoal){
+        return null;
+    }
+    //IterativeDeepeningDepthFirstSearch
+    public List searchForSword(Node n, Node goal, List path){
+        path.add(n);
+        for(Node l : n.children){
+            if (l == goal){
+                System.out.println("Found room");
+                return path;
+            }
         }
+        for(Node l : n.children){
+            return searchForSword(l, goal, path);
+        }
+        return null;
     }
 }
